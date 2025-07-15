ad34ee773764ce6851c33c710d0d8f3899800540
enda <nghiavee@gmail.com>
2025-07-15 11:40:22 +0700

    update session 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,12 @@
-// header //
-
-// mở/đóng menu ở màn hình moblie
-document.getElementById("btn-menu")
-    .addEventListener("click", function() 
-    {
-        var nav = document.getElementById("nav");
-        if (nav.classList.contains("show"))
-            nav.classList.remove("show");
-        else
-            nav.classList.add("show");
-    });
-
+var toggle = document.getElementById("toggle");
+var mainnav = document.getElementById("mainnav");
+toggle.onclick = function(){
+    mainnav.classList.toggle('active');
+    document.querySelector('.inner-logo').classList.toggle('hide-when-menu-open');
+    document.querySelector('.inner-shopping-cart').classList.toggle('hide-when-menu-open');
+    document.querySelector('.inner-wrap').classList.toggle('inner-wrap-change');
+    document.querySelector('.inner-header-btn').classList.toggle('inner-header-btn-change');
+    document.querySelector('.fa-bars').classList.toggle('exchange-after-click');
+    document.querySelector('.fa-x').classList.toggle('exchange-after-click');
+    document.querySelector('.header').classList.toggle('active');
+}
